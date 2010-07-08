ef1541ace518345737d66713c27afd0cde754976
neilrqm@gmail.com <neilrqm@gmail.com@3c1b0362-c62b-8bf9-9a54-5033ff64f510>
2010-07-08 01:06:47 +0000

    Added code for HMC6352 compass.

---
+++ trunk/L_Shared/compass/compass.cpp
@@ -0,0 +1,56 @@
+/*
+ * compass.cpp
+ *
+ *  Created on: 6-Jul-2010
+ *      Author: nrqm
+ *
+ * This code owes a lot to http://www.arduino.cc/playground/Learning/Hmc6352 .
+ */
+
+#include "compass.h"
+#include "../arduino/WProgram.h"
+#include "../arduino/Wire.h"
+#include "../roomba/roomba.h"
+
+#define ADDRESS (0x42 >> 1)
+
+void send_command(const char* command)
+{
+	Wire.beginTransmission(ADDRESS);
+	Wire.send((char*)command);
+	Wire.endTransmission();
+}
+
+void Compass_Init()
+{
+	Wire.begin();
+}
+
+void Compass_Calibrate()
+{
+	send_command("C");
+	delay(20000);
+	send_command("E");
+	delay(15);
+}
+
+uint16_t Compass_GetReading()
+{
+	uint8_t i;
+	uint16_t headingValue;
+	uint8_t headingData[2];
+	// send "get data" command and wait for at least 6 ms
+	send_command("A");
+	delay(10);
+
+	i = 0;
+	Wire.requestFrom(ADDRESS, 2);        // Request the 2 byte heading (MSB comes first)
+	// nrqm - I think this loop is bad, should be replaced
+	while(Wire.available() && i < 2)
+	{
+		headingData[i] = Wire.receive();
+		i++;
+	}
+	headingValue = headingData[0]*256 + headingData[1];
+	return headingValue;
+}
---
+++ trunk/L_Shared/compass/compass.h
@@ -0,0 +1,38 @@
+/*
+ * compass.h
+ *
+ *  Created on: 6-Jul-2010
+ *      Author: nrqm
+ */
+
+#ifndef COMPASS_H_
+#define COMPASS_H_
+
+#include <avr/io.h>
+
+/**
+ * Initialize the compass unit and start the TWI module
+ */
+void Compass_Init();
+
+/**
+ * Calibrate the compass.  This function puts the compass into calibration mode, waits for 20 seconds,
+ * exits calibration mode, and waits for another 15 ms.  The calibration routine requires that the
+ * compass be held flat while rotating (the datasheet suggests 2 full rotations in 20 seconds).  As such,
+ * the compass must start rotating in place before this function is called.
+ *
+ * The calibration routine corrects for hard iron distortions in the magnetic field.  The calibration
+ * results are stored in EEPROM.  Therefore this routine only needs to be run when the hard iron distortion
+ * (e.g. from pieces of ferromagnetic or magnetic material) placed at a constant position relative to the
+ * compass changes.
+ */
+void Compass_Calibrate();
+
+/**
+ * Query the compass for a reading.  This function includes a 10 ms delay.  It returns a number from 0 to
+ * 3599, representing the compass's orientation clockwise from magnetic north, in increments of 0.1 degree.
+ * In other words, the range is 0 to 359.9 degrees.
+ */
+uint16_t Compass_GetReading();
+
+#endif /* COMPASS_H_ */
