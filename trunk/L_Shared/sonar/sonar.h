1daa0a0f2ab0907081d2280dbb774db5958f6a52
neilrqm@gmail.com <neilrqm@gmail.com@3c1b0362-c62b-8bf9-9a54-5033ff64f510>
2011-08-06 20:16:54 +0000

    Simplified sonar transmitting code.

--- trunk/L_Shared/sonar/sonar.h
+++ trunk/L_Shared/sonar/sonar.h
@@ -10,39 +10,27 @@
 #define SONAR_H_
 
 /**
- * This module provides programmatic access to the SRF04 ultrasonic ranger.  It assumes the following
- * wiring:
+ * This module provides programmatic access to the Seeed Studio ultrasonic ranger.
+ * It assumes the following wiring on the Arduion Pro Mini:
  *
- *     Sonar                Seeeduino
+ *     Sonar                Arduino        Arduino Mega
  *
- * 		Vcc ----------------- +5V
- *   Output ----------------- PD4
- *  Trigger ----------------- Arduino pin 10 (PB4)
- *     (NC) -----------------
- *      GND ----------------- GND
+ * 		Vcc ----------------- +5V             +5V
+ *   Signal ----------------- Pin 8          Pin 48
+ *      GND ----------------- GND             GND
  *
- * An Arduino pin number is not assigned to PD4, it is labelled "PD4" on the Seeeduino headers.
  */
-
-typedef enum _ss
-{
-	IDLE,
-	WAITING_FOR_PULSE_START,	/// Trigger has been sent to sonar
-	WAITING_FOR_PULSE_END,		/// Sonar echo pulse has started, waiting for falling edge
-	OUTPUT_READY,				/// Echo pulse is done, ready to log results.
-} SONAR_STATE;
 
 /**
  * Initialise the trigger pin and the input capture timer.
  *
- * This must be called after the Arduino init() function or else init() will clobber the timer settings.
+ * This must be called after the Arduino init() function or else init() will clobber our timer settings.
  */
 void Sonar_Init();
 
 /**
  * Set the sonar trigger pin high, and initialise the driver's state.  The sonar will not be triggered
- * until Sonar_Trigger() is called.  After this is called Sonar_GetState() will return something
- * other than OUTPUT_READY.
+ * until Sonar_Trigger() is called.
  */
 void Sonar_PreTrigger();
 
@@ -53,13 +41,9 @@ void Sonar_PreTrigger();
 void Sonar_Trigger();
 
 /**
- * Get the current state of the sonar driver (see SONAR_STATE enumeration).
- */
-SONAR_STATE Sonar_GetState();
-
-/**
- * Get the distance measured by the sonar.  If the sonar is currently measuring a signal (i.e. Sonar_GetState()
- * returns something other than OUTPUT_READY) then Sonar_GetDistance() returns 0.
+ * Get the last distance measured by the sonar.
+ * This should be called after the pulse-end callback sonar_pulseend_cb has been asserted by the sonar module.
+ * The distance calculation takes a long time, so this should not be called within the callback.
  */
 uint16_t Sonar_GetDistance(int8_t error_in_cm);
 
